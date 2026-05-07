ebc9fbc1498eafb35279cda399ca410fca7bf2a6
JAHemmings <jaredandreh@gmail.com>
2026-05-07 02:30:19 -0600

    Update Linea.java

--- Linea.java
+++ Linea.java
@@ -1,5 +1,32 @@
 package POO_Tienda;
 
 public class Linea {
+    private double cantidad;
+    private Producto producto;
 
+    public Linea(double cantidad, Producto producto) {
+        this.cantidad = cantidad;
+        this.producto = producto;
+    }
+
+    public double calcularSubtotal() {
+        return cantidad * producto.getPrecio();
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public double getCantidad() {
+        return cantidad;
+    }
+
+    @Override
+    public String toString() {
+        return "Linea{" +
+                "cantidad=" + cantidad +
+                ", producto=" + producto.getNombre() +
+                ", subtotal=" + calcularSubtotal() +
+                '}';
+    }
 }
